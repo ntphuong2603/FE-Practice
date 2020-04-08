f935c0ab632ccc6eb48f39a817fd97c08334784a
Thanh Phuong Nguyen <thanhphuong@ubuntu>
2020-04-08 13:20:49 -0300

    Update NavBar.js

--- reactjs-01/src/components/NavBar.js
+++ reactjs-01/src/components/NavBar.js
@@ -5,11 +5,18 @@ class NavBar extends Component {
     constructor(props){
         super(props);
         this.state = {
-            active:[]
+            activeMenu: []
         }
     }
 
-    menuBar = ['Book', 'Author', 'Genre', 'Book-Instance'].map((item)=>{
+    menuActive = (index) => {
+        return(
+            null
+        )
+    }
+    
+    menuBar = ['Book', 'Author', 'Genre', 'Book-Instance'].map((item,index)=>{
+        this.setState({activeMenu: new Array(4).fill(false)})
         return(
             <Nav.Item>
                 <Nav.Link href={`/${item.toLowerCase()}`}>{item}</Nav.Link>
